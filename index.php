b4248d58f11037bb214bb7cbd563f41b8cce760f
SaguidSunshine <saguidsunshine03@gmail.com>
2024-03-20 13:42:46 +0800

    Added index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Favorite Songs</title>
+</head>
+<body>
+    <h1>My Favorite Songs</h1>
+    <ul>
+        <li><a href="song1.php">Pink Skies</a></li>
+        <li><a href="song2.php">Peaches</a></li>
+        <li><a href="song3.php">Stay</a></li>
+        <li><a href="song4.php">Best Part</a></li>
+        <li><a href="song5.php">Get You</a></li>
+    </ul>
+</body>
+</html>
