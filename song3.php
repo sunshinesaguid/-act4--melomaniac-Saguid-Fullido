e694d53a336d9da0c90676ba4fb9d561cb41192f
SaguidSunshine <saguidsunshine03@gmail.com>
2024-03-20 13:44:48 +0800

    Added song3.php

---
+++ song3.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stay - The Kid LAROI & Justin Bieber</title>
+</head>
+<body>
+    <h1>Stay - The Kid LAROI & Justin Bieber</h1>
+
+    <h2>Chorus</h2>
+    <?php
+    echo "<p>F                     G<br>";
+    echo "I do the same thing I told you that I never would<br>";
+    echo "  Am                        Em<br>";
+    echo "I told you I'd change, even when I knew I never could<br>";
+    echo "  F                        G<br>";
+    echo "I know that I can't find nobody else as good as you<br>";
+    echo "  Am                            N.C.<br>";
+    echo "I need you to stay, need you to stay, hey (Oh)</p>";
+    ?>
+
+    <h2>Verse 1</h2>
+    <?php
+    echo "<p>F            G                   Am<br>";
+    echo "I get drunk, wake up, I'm wasted still<br>";
+    echo "                  Em<br>";
+    echo "I realize the time that I wasted here<br>";
+    echo "F               G                    Am<br>";
+    echo "I feel like you can't feel the way I feel<br>";
+    echo "                      Em<br>";
+    echo "Oh, I'll be fucked up if you can't be right here</p>";
+    ?>
+
+    <h2>Pre-Chorus</h2>
+    <?php
+    echo "<p>F           G<br>";
+    echo "Oh, ooh-woah (Oh, ooh-woah, ooh-woah)<br>";
+    echo "Am          Em<br>";
+    echo "Oh, ooh-woah (Oh, ooh-woah, ooh-woah)<br>";
+    echo "F           G<br>";
+    echo "Oh, ooh-woah (Oh, ooh-woah, ooh-woah)<br>";
+    echo "Am                     N.C.<br>";
+    echo "I'll be fucked up if you can't be right here</p>";
+    ?>
+
+    <h2>Verse 2</h2>
+    <?php
+    echo "<p>N.C.<br>";
+    echo "When I'm away from you I miss your touch (Ooh)<br>";
+    echo "Am                       Em<br>";
+    echo "  You're the reason I believe in love (Ooh)<br>";
+    echo "F                        G<br>";
+    echo " It's been difficult for me to trust (Ooh)<br>";
+    echo "Am                           Em<br>";
+    echo "  And I'm afraid that I'ma fuck it up (Ooh)<br>";
+    echo "F                          G<br>";
+    echo "  Ain't no way that I can leave you stranded<br>";
+    echo "Am                               Em<br>";
+    echo "  'Cause you ain't ever left me empty-handed<br>";
+    echo "F                     G<br>";
+    echo "  And you know that I know that I can't live without you<br>";
+    echo "          N.C.<br>";
+    echo "So, baby, stay</p>";
+    ?>
+
+    
+</body>
+</html> 
