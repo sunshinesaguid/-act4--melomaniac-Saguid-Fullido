f8cab1f8d46b9ab83a959bea5bb48b1a85cb171c
SaguidSunshine <saguidsunshine03@gmail.com>
2024-03-20 13:44:03 +0800

    Added song2.php

---
+++ song2.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Peaches - Justin Bieber ft. Daniel Caesar & Giveon</title>
+</head>
+<body>
+    <h1>Peaches - Justin Bieber ft. Daniel Caesar & Giveon</h1>
+
+    <h2>Chorus</h2>
+    <?php
+    echo "<p>                        Fmaj7<br>";
+    echo "I got my peaches out in Georgia (Oh, yeah, shit)<br>";
+    echo "                        Em7<br>";
+    echo "I get my weed from California (That's that shit)<br>";
+    echo "                          Dm7<br>";
+    echo "I took my chick up to the North, yeah (Badass bitch)<br>";
+    echo "                              Cmaj7<br>";
+    echo "I get my light right from the source, yeah (Yeah, that’s it)</p>";
+    ?>
+
+    <h2>Verse 1</h2>
+    <?php
+    echo "<p>          Fmaj7                          Em7<br>";
+    echo "And I say oh (oh), the way I breathe you in (In)<br>";
+    echo "                         Dm7<br>";
+    echo "It's the texture of your skin<br>";
+    echo "<br>";
+    echo "I wanna wrap my arms around you, babe<br>";
+    echo "    Cmaj7<br>";
+    echo "And never let you go, oh<br>";
+    echo "           Fmaj7                         Em7<br>";
+    echo "And I say, oh, there's nothin' like your touch<br>";
+    echo "                         Dm7<br>";
+    echo "It’s the way you lift me up, yeah<br>";
+    echo "            Cmaj7<br>";
+    echo "And I'll be right here with you 'til the end</p>";
+    ?>
+
+    <h2>Verse 2</h2>
+    <?php
+    echo "<p>Fmaj7               Em7<br>";
+    echo "  You ain't sure yet, but I'm for ya'<br>";
+    echo "Dm7                           Cmaj7<br>";
+    echo "  All I could want, all I can wish for<br>";
+    echo "<br>";
+    echo "Nights alone that we miss more<br>";
+    echo "Fmaj7<br>";
+    echo "  And days we save as souvenirs<br>";
+    echo "Em7                                  Dm7<br>";
+    echo "  There’s no time, I wanna make more time<br>";
+    echo "                      Cmaj7<br>";
+    echo "And give you my whole life<br>";
+    echo "                          Fmaj7   Em7<br>";
+    echo "I left my girl, I’m in my Yorker<br>";
+    echo "                           Dm7   Cmaj7<br>";
+    echo "Hate to leave her, call it torture<br>";
+    echo "                         Fmaj7   Em7<br>";
+    echo "Remember when I couldn't hold her<br>";
+    echo "                       Dm7   Cmaj7<br>";
+    echo "Left her baggage for Rimowa</p>";
+    ?>
+
+</body>
+</html> 
