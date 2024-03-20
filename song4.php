bbdfec06f54b5d65084509dea762bdf0a3b6289e
SaguidSunshine <saguidsunshine03@gmail.com>
2024-03-20 13:45:27 +0800

    Added song4.php

---
+++ song4.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Best Part - Daniel Caesar ft. H.E.R.</title>
+</head>
+<body>
+    <h1>Best Part - Daniel Caesar ft. H.E.R.</h1>
+
+    <h2>Verse 1</h2>
+    <?php
+    echo "<p>Dmaj7<br>";
+    echo "You don't know babe<br>";
+    echo "Am7<br>";
+    echo "When you hold me<br>";
+    echo "Gmaj7<br>";
+    echo "And kiss me slowly<br>";
+    echo "         Bbmaj7<br>";
+    echo "It's the sweetest thing<br>";
+    echo "Dmaj7<br>";
+    echo "And it don't change<br>";
+    echo "Am7<br>";
+    echo "If I had it my way<br>";
+    echo "Gmaj7                   Bbmaj7<br>";
+    echo "You would know that you are</p>";
+    ?>
+
+    <h2>Pre-Chorus 1</h2>
+    <?php
+    echo "<p>Dmaj7<br>";
+    echo "You're the coffee that I need in the morning<br>";
+    echo "Am7<br>";
+    echo "You're my sunshine in the rain when it's pouring<br>";
+    echo "Gmaj7<br>";
+    echo "Won't you give yourself to me<br>";
+    echo "        Bbmaj7<br>";
+    echo "Give it all, oh</p>";
+    ?>
+
+    <!-- Continue with Chorus, Verse 2, Pre-Chorus 2, and Outro -->
+
+    <h2>Chorus</h2>
+    <?php
+    echo "<p>             Dmaj7<br>";
+    echo "I just wanna see<br>";
+    echo "               Am7<br>";
+    echo "I just wanna see how beautiful you are<br>";
+    echo "Gmaj7             Bbmaj7<br>";
+    echo "You know that I see it<br>";
+    echo "                 Dmaj7<br>";
+    echo "I know you're a star<br>";
+    echo "               Am7<br>";
+    echo "Where you go I follow<br>";
+    echo "No matter how far<br>";
+    echo "Gmaj7         Bbmaj7<br>";
+    echo "If life is a movie<br>";
+    echo "                      Dmaj7 Am7<br>";
+    echo "Oh you're the best part, oh<br>";
+    echo "                Gmaj7    Bbmaj7<br>";
+    echo "You're the best part, oh<br>";
+    echo "Best part</p>";
+    ?>
+
+    <!-- Continue with Verse 2, Pre-Chorus 2, Chorus, and Outro -->
+
+</body>
+</html>
