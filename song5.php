3a464a3c60a904e7c66efe87eb28b570cbb288de
SaguidSunshine <saguidsunshine03@gmail.com>
2024-03-20 13:46:34 +0800

    Added song5.php

---
+++ song5.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get You- Lauv ft. BTS</title>
+</head>
+<body>
+    <h1>Get You - Lauv ft. BTS</h1>
+
+    <h2>Verse 1</h2>
+    <?php
+    echo "<p>Em                  F#m7               Bm<br>";
+    echo "Through drought and famine, natural disasters<br>";
+    echo "            A7                 Em<br>";
+    echo "My baby has been around for me<br>";
+    echo "              F#m7              Bm<br>";
+    echo "Kingdoms have fallen, angels be calling<br>";
+    echo "                   A7            Em<br>";
+    echo "None of that could ever make me leave, yeah<br>";
+    echo "F#m7                        Bm          A7<br>";
+    echo "Every time I look into your eyes I see it<br>";
+    echo "             Em<br>";
+    echo "You're all I need<br>";
+    echo "F#m7                      Bm           A7<br>";
+    echo "Every time I get a bit inside I feel it</p>";
+    ?>
+
+    <h2>Chorus</h2>
+    <?php
+    echo "<p>Em       F#m7             Bm               A7<br>";
+    echo "Oooooooh, who would've thought I'd get you<br>";
+    echo "Em       F#m7             Bm               A7<br>";
+    echo "Oooooooh, who would've thought I'd get you</p>";
+    ?>
+
+    <h2>Verse 2</h2>
+    <?php
+    echo "<p>Em               F#m7<br>";
+    echo "And when we're making love<br>";
+    echo "Bm                     A7                 Em<br>";
+    echo "Your cries they can be heard from far and wide<br>";
+    echo "              F#m7          Bm<br>";
+    echo "It's only the two of us<br>";
+    echo "              A7                  Em<br>";
+    echo "Everything I need's between those thighs<br>";
+    echo "F#m7                        Bm          A7<br>";
+    echo "Every time I look into your eyes I see it<br>";
+    echo "             Em<br>";
+    echo "You're all I need<br>";
+    echo "F#m7                      Bm           A7<br>";
+    echo "Every time I get a bit inside I feel it</p>";
+    ?>
+
+    <!-- Continue with Verse 3, Bridge, Chorus, and Verse 4 -->
+
+    <h2>Verse 3</h2>
+    <?php
+    echo "<p>Em                   F#m7<br>";
+    echo "And I’ll take some time<br>";
+    echo "                Bm<br>";
+    echo "Just to be thankful<br>";
+    echo "               A7                Em<br>";
+    echo "That I had days full of you, you<br>";
+    echo "          F#m7<br>";
+    echo "Before it winds down into<br>";
+    echo "    Bm                      A7<br>";
+    echo "The memories, It’s all just memories</p>";
+    ?>
+
+    <h2>Bridge</h2>
+    <?php
+    echo "<p>Em<br>";
+    echo "(If you've got someone you like...)<br>";
+    echo "F#m                            Bm         A7<br>";
+    echo "Don't you love when I come around<br>";
+    echo "(...feel something that's right somebody just tell somebody)<br>";
+    echo "A7                            Em<br>";
+    echo "Build you up then I take you down<br>";
+    echo "(If you've got someone you like...)<br>";
+    echo "F#m                            Bm         A7<br>";
+    echo "Don't you love when I come around<br>";
+    echo "(...feel something that's right somebody just tell somebody)<br>";
+    echo "A7<br>";
+    echo "Build you up then I take...</p>";
+    ?>
+
+    <!-- Continue with Chorus, Verse 4 -->
+
+    <h2>Chorus</h2>
+    <?php
+    echo "<p>Em       F#m7                             Bm<br>";
+    echo "Oooooooh, who would've thought I'd get you<br>";
+    echo "   A7<br>";
+    echo "Oh yeah, oh yeah baby<br>";
+    echo "Em       F#m7                             Bm    A7<br>";
+    echo "Oooooooh, who would've thought I'd get you</p>";
+    ?>
+
+    <h2>Verse 4</h2>
+    <?php
+    echo "<p>Em                 F#m7<br>";
+    echo "This feels like summer<br>";
+    echo "        Bm                 A7<br>";
+    echo "Boy you make me feel so alive<br>";
+    echo "Em           F#m7<br>";
+    echo "Just be my lover<br>";
+    echo "           Bm              Em<br>";
+    echo "Boy you'll lead me to paradise</p>";
+    ?>
+
+</body>
+</html>
