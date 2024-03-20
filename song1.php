e822f8964dd15eae497a9a4a99c8caace766a3b5
SaguidSunshine <saguidsunshine03@gmail.com>
2024-03-20 13:43:21 +0800

    added song1.php

---
+++ song1.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pink Skies - Lyrics and Chords</title>
+</head>
+<body>
+    <h1>Pink Skies</h1>
+    <h2>by LANY</h2>
+    
+    <h3>[Verse 1]</h3>
+    <?php
+    echo '<p>Dm7          C</p>';
+    echo '<p>Thrift store fashion.</p>';
+    echo '<p>Am7     Bb</p>';
+    echo '<p>Imperfect tattoos.</p>';
+    echo '<p>Dm7    C</p>';
+    echo '<p>Taking showers,</p>';
+    echo '<p>Am7       Bb</p>';
+    echo '<p>Minus shampoo.</p>';
+    ?>
+    
+    <h3>[Pre-Chorus]</h3>
+    <?php
+    echo '<p>Dm7        C        Am7  Bb</p>';
+    echo '<p>You are my favorite everything.</p>';
+    echo '<p>Gm11               Am7              Bb       C</p>';
+    echo '<p>Been telling girls that since I was sixteen.</p>';
+    echo '<p>Dm7        C</p>';
+    echo '<p>Shut up, I love you,</p>';
+    echo '<p>Am7       Bb</p>';
+    echo '<p>You\'re my best friend.</p>';
+    ?>
+
+    <h2>Verse 1</h2>
+    <?php
+    echo "<p>Dm7          C<br>";
+    echo "Thrift store fashion.<br>";
+    echo "  Am7     Bb<br>";
+    echo "Imperfect tattoos.<br>";
+    echo "Dm7    C<br>";
+    echo "Taking showers,<br>";
+    echo "Am7       Bb<br>";
+    echo "Minus shampoo.</p>";
+    ?>
+
+    <h2>Pre-Chorus</h2>
+    <?php
+    echo "<p>Dm7        C        Am7  Bb<br>";
+    echo "You are my favorite everything.<br>";
+    echo "Gm11               Am7              Bb       C<br>";
+    echo "Been telling girls that since I was sixteen.<br>";
+    echo "Dm7        C<br>";
+    echo "Shut up, I love you,<br>";
+    echo "Am7       Bb<br>";
+    echo "You're my best friend.</p>";
+    ?>
+
+    <h2>Bridge</h2>
+    <?php
+    echo "<p>F              C<br>";
+    echo "We can work it out.<br>";
+    echo "        Am7           Bb<br>";
+    echo "You and I are meant to be together.<br>";
+    echo "F                   C<br>";
+    echo "This is how it's supposed to feel.<br>";
+    echo "Am7         Bb<br>";
+    echo "I'm in love with how this feels.</p>";
+    ?>
+
+    <h2>Chorus</h2>
+    <?php
+    echo "<p>F             C                  Am7              Bb<br>";
+    echo "Get you under pink skies, I know exactly where we should go.<br>";
+    echo "F                         C                        Am7          Bb<br>";
+    echo "'Cause I love the way your green eyes mix with that Malibu indigo.<br>";
+    echo "F             C                       Am7                    Bb<br>";
+    echo "Talking under pink skies, I think our hearts are starting to show.<br>";
+    echo "F                         C  Am7        Bb<br>";
+    echo "That it's better, You and I, under pink skies.</p>";
+    ?>
+
+</body>
+</html>
+    
